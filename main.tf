47c71ddb2ca419a576c993c9cbe5bb5eb579bb0c
unknown <adeniyifunmilola77@gmail.com>
2025-07-07 03:36:32 -0700

    Initial commit: EC2 instance with Terraform

---
+++ main.tf
@@ -0,0 +1,17 @@
+# main.tf
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAWLIS4GLWE5EE7A45"
+  secret_key = "TYouPobtNdZQRSDhstTTlqRb7lHgq8eMRclO6Cl8"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-000ec6c25978d5999"
+  instance_type = "t2.micro"
+  key_name      = "linux-keypair"
+
+  tags = {
+    Name = "Hello-mine"
+  }
+}
