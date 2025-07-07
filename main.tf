06941205ba5b318a77376b078422790994a19fe3
unknown <adeniyifunmilola77@gmail.com>
2025-07-07 05:36:20 -0700

    Added multiple EC2 instances to Terraform configuration

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@
 
 provider "aws" {
   region     = "us-east-1"
-
+  access_key = "AKIAWLIS4GLWE5EE7A45"
+  secret_key = "TYouPobtNdZQRSDhstTTlqRb7lHgq8eMRclO6Cl8"
 }
     resource "aws_instance" "example" {
   ami           = "ami-000ec6c25978d5999"
