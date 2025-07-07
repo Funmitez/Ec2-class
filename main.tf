755c91d20ddbdc2eb51bb960140772fbf70abd78
unknown <adeniyifunmilola77@gmail.com>
2025-07-07 05:26:29 -0700

    Added multiple EC2 instances to Terraform configuration

--- main.tf
+++ main.tf
@@ -2,33 +2,24 @@
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAWLIS4GLWE5EE7A45"
-  secret_key = "TYouPobtNdZQRSDhstTTlqRb7lHgq8eMRclO6Cl8"
-}
 
-resource "aws_instance" "example" {
+}
+    resource "aws_instance" "example" {
   ami           = "ami-000ec6c25978d5999"
   instance_type = "t2.micro"
   key_name      = "linux-keypair"
 
   tags = {
-    Name = "Hello-mine"
+    Name = "MyInstance"
   }
 }
-resource "aws_instance" "example" 
+resource "aws_instance" "example_2" {
   ami           = "ami-000ec6c25978d5999"
   instance_type = "t2.micro"
   key_name      = "linux-keypair"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
-    Name = "Hello-mine-2"
+    Name = "MyInstance-2"
   }
- 
- resource "aws_instance" "example" 
-  ami           = "ami-000ec6c25978d5999"
-  instance_type = "t2.micro"
-  key_name      = "linux-keypair"
-
-  tags = {
-    Name = "Hello-mine-2"
-  } 
+}
